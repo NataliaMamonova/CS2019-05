7c588dac2e5420a6be37eda3c7bb31a39fd13885
natallia <natallia-l@yandex.ru>
2019-04-30 16:18:28 +0300

    Задания первого урока Мамонова

---
+++ src/n/mamonova/Lesson2/zadanie2.java
@@ -0,0 +1,11 @@
+package n.mamonova.Lesson2;
+
+public class zadanie2 {
+    public static void main (String[] args){
+        String S = "Я изучаю Java";
+        System.out.println (S);
+        System.out.println (S);
+        System.out.println (S);
+        System.out.println (S);
+    }
+}
---
+++ src/n/mamonova/lesson1/task3.java
@@ -0,0 +1,12 @@
+package n.mamonova.lesson1;
+
+public class task3 {
+
+        public static void main (String[] args){
+            int i=3;
+            int j=4;
+            int k=i*i+j*j;
+            System.out.println (i+"*"+i+"+"+j+"*"+j+"="+k);
+                    }
+    }
+
